0695ca193cca0a599013ef8124bd2f825d3ec9e4
Luis Mayta <luis.mayta@gmail.com>
2017-03-20 22:53:47 -0500

    Cambios en rutas

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,9 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 require __DIR__.'/home.route.php';
 require __DIR__.'/admin.route.php';
 require __DIR__.'/catalogo.route.php';
 require __DIR__.'/users.route.php';
 
 Auth::routes();
-
-//Route::get('/home', 'HomeController@index');
